fe5ca80470df35933a58dbde2cecb759fb0397cd
Addeballe <adrian.nilsson@elev.ga.ntig.se>
2026-03-13 10:17:54 +0100

    add bubble sort implementation with performance timing

--- bubblesort.py
+++ bubblesort.py
@@ -4,27 +4,20 @@ import time
 def bubbleSort(arr):
     n = len(arr)
     
-    # Traverse through all array elements
     for i in range(n):
         swapped = False
-        # Last i elements are already in place
         for j in range(0, n-i-1):
-            # Traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 swapped = True
         if (swapped == False):
             break
 
-# Driver code to test above
 arr = []
-for y in range(10000):
+for y in range(25000):
     arr.append(random.randint(1, 10000))
 
+start_timer = time.time()
 bubbleSort(arr)
 
-print("Sorted array:")
-for i in range(len(arr)):
-    print("%d" % arr[i], end=" ")
+print(time.time() - start_timer)
--- super.test.py
+++
@@ -1,5 +0,0 @@
-import time
-start_time = time.time()
-time.sleep(0.5)
-stop_timer = time.time()
-print(start_time - stop_timer)
